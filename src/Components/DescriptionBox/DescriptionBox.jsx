55ee4a5d5d181287d7da3360fdc2692e9faa8659
muhammed seryul <arif@arif.ser>
2023-12-03 23:13:53 +0300

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+    <div className='descriptionbox-navigator'>
+    <div className='descriptionbox-nav-box'>Description</div>
+    <div className='descriptionbox-nav-box fade'>Reviws (122)</div>
+    </div>
+      <div className='descriptionbox-description'>
+      <p>
+      In academic terms, a text is anything that conveys a set 
+      of meanings to the person who examines it. You might have
+       thought that texts were limited to written materials, such
+        as books, magazines, newspapers, and zines an informal 
+            term for magazine that refers especially to fanzines and
+              Those items are indeed texts—but so are movies
+              paintings, television shows, songs, political cartoons,
+              online materials, advertisements, maps, works of art, and 
+              even rooms full of people. If we can look at something, 
+              explore it, find layers of meaning in it, and draw information 
+              and conclusions from it, were looking at a text.
+      </p>
+      <p>
+      In academic terms, a text is anything that conveys a set of 
+      meanings to the person who examines it. You might have thought
+       that texts were limited to written materials, such as books,
+        magazines, newspapers, and zines an informal term for magazine
+             that refers especially to fanzines and webzinesThose 
+             items are indeed texts—but so are movies, paintings, television
+              shows, songs, political cartoons, online materials, advertisements,
+               maps, works of art, and even rooms full of people. If we can look 
+               at something, explore it, find layers of meaning in it, and draw
+                information and conclusions from it, were looking at a text.
+      </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
